5a86c30f882efc80d0e7b6314ef35277a5cc2f2a
shaoyuchu <b06705028@ntu.edu.tw>
2022-05-26 23:12:28 +0800

    add kruskal-wallis and Dunn's test

--- data_analysis/analysis.py
+++ data_analysis/analysis.py
@@ -2,6 +2,8 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
+from scikit_posthocs import posthoc_dunn
 
 CONDITIONS = [
     "Unordered Characters (in-word)",
@@ -39,6 +41,12 @@ def get_by_condition_correctness(para_correctness: pd.DataFrame):
     df[f"{CONDITIONS[1]} *6"] = para_correctness.loc[:, 13:16].mean(axis=1)
     df[f"{CONDITIONS[2]} *3"] = para_correctness.loc[:, 17:20].mean(axis=1)
     df[f"{CONDITIONS[2]} *6"] = para_correctness.loc[:, 21:24].mean(axis=1)
+
+    for cond_id in range(3):
+        for swap_id in range(2):
+            i = (cond_id * 2 + swap_id) * 4 + 1
+            col_name = f"{CONDITIONS[cond_id]} *{SWAP_NUM[swap_id]}"
+            df[col_name] = para_correctness.loc[:, i : i + 3].mean(axis=1)
     return df
 
 
@@ -104,7 +112,7 @@ def histogram_by_num_swap(cond_correctness: pd.DataFrame, image_dir: Path):
     plt.style.use("ggplot")
     bins = np.linspace(0, 1, num=8)
     n_user = len(cond_correctness)
-    for swap_num in ["3", "6"]:
+    for swap_num in SWAP_NUM:
         plt.clf()
         plt.title(f"{swap_num} Swaps")
         for i in range(3):
@@ -123,6 +131,47 @@ def histogram_by_num_swap(cond_correctness: pd.DataFrame, image_dir: Path):
         plt.savefig(image_dir / f"Histogram - {swap_num} swaps.png", dpi=300)
 
 
+def print_statistics(cond_correctness: pd.DataFrame):
+    for cond in CONDITIONS:
+        for swap_num in SWAP_NUM:
+            col_name = f"{cond} *{swap_num}"
+            data = cond_correctness[col_name]
+            mean = np.mean(data)
+            std = np.std(data)
+            print(f"{col_name}:\tmean = {'%.2f'%mean}, std = {'%.2f'%std}")
+
+
+def compare_median(cond_correctness: pd.DataFrame):
+    """Run Kruskal-Wallis test and Dunn's test for post-hoc test.
+
+    Test if the median of the correctness is the same for different swap types.
+    Note that the statistics are not normally distributed, so we use
+    non-parametric tests.
+    """
+    for swap_num in SWAP_NUM:
+        result = stats.kruskal(
+            *[
+                cond_correctness[f"{CONDITIONS[i]} *{swap_num}"]
+                for i in range(3)
+            ],
+        )
+        print(
+            f"\nKruskal-Wallis test for {swap_num} swaps: "
+            f"p-value = {'%.2e'%result.pvalue}"
+        )
+
+        # post hoc Dunn's test with Bonferroni correction
+        if result.pvalue < 0.05:
+            post_hoc = posthoc_dunn(
+                [
+                    cond_correctness[f"{CONDITIONS[i]} *{swap_num}"]
+                    for i in range(3)
+                ],
+                p_adjust="bonferroni",
+            )
+            print("post-hoc Dunn's test:\n", post_hoc < 0.05)
+
+
 if __name__ == "__main__":
     data_dir = Path("data")
     image_dir = Path("image")
@@ -131,9 +180,5 @@ if __name__ == "__main__":
     scatter_plot(cond_correctness, image_dir)
     histogram_by_swap_type(cond_correctness, image_dir)
     histogram_by_num_swap(cond_correctness, image_dir)
-    for cond in CONDITIONS:
-        for num in [" *3", " *6"]:
-            data = cond_correctness[cond + num]
-            mean = np.mean(data)
-            std = np.std(data)
-            print(f"{cond + num}:\tmean = {'%.2f'%mean}, std = {'%.2f'%std}")
+    print_statistics(cond_correctness)
+    compare_median(cond_correctness)
